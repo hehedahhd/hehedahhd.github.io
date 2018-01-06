0f04e8ed736fd4405c390eba4fd9173ad52f9c34
Hhdº <32481234+hehedahhd@users.noreply.github.com>
2018-01-06 16:43:37 +0800

    Update IFSafari.js

--- js/IFSafari.js
+++ js/IFSafari.js
@@ -1,3 +1,4 @@
 		if(navigator.userAgent.indexOf("Safari") == -1){
-			alert("需在Safari访问才能安装！\n请点击右上角，选择“在Safari打开“")
+document.getElementById("Page").innerHTML("<div class="Message Message--red"> <div class="Message-icon"> <i class="fa fa-times"></i> </div> <div class="Message-body"> <p>在Safari打开才能安装！</p> </div> <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
+</div>")
 		}
