40a536656b1186e65836813a055f8f1112a15eef
Hhdº <32481234+hehedahhd@users.noreply.github.com>
2018-01-06 16:47:51 +0800

    Update IFSafari.js

--- js/IFSafari.js
+++ js/IFSafari.js
@@ -1,4 +1,4 @@
 		if(navigator.userAgent.indexOf("Safari") == -1){
-document.getElementById("Page").innerHTML("<div class="Message Message--red"> <div class="Message-icon"> <i class="fa fa-times"></i> </div> <div class="Message-body"> <p>在Safari打开才能安装！</p> </div> <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
+document.getElementById("Page").innerHTML("<div class='Message Message--red'> <div class='Message-icon'> <i class='fa fa-times'></i> </div> <div class='Message-body'> <p>在Safari打开才能安装！</p> </div> <button class='Message-close js-messageClose'><i class='fa fa-times'></i></button>
 </div>")
 		}
